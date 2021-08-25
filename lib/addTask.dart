99d18c9814b3afa2a5b8d043630888950bdc60b3
Kanzoonrkz <kanzoonrkz@gmail.com>
2021-08-25 21:55:17 +0700

    add dependencies for theme setup

--- lib/addTask.dart
+++ lib/addTask.dart
@@ -12,43 +12,44 @@ class AddTask extends StatelessWidget {
       ),
       onPressed: () {
         showDialog(
-            context: context,
-            builder: (BuildContext context) {
-              return AlertDialog(
-                scrollable: true,
-                content: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                    child: Column(
-                      children: <Widget>[
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Task Name',
-                          ),
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              scrollable: true,
+              content: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Form(
+                  child: Column(
+                    children: <Widget>[
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Task Name',
                         ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Category',
-                          ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Category',
                         ),
-                        TextFormField(
-                          decoration: InputDecoration(
-                            labelText: 'Time',
-                          ),
+                      ),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Time',
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ),
-                actions: [
-                  ElevatedButton(
-                      child: Text("Add Task"),
-                      onPressed: () {
-                        // your code
-                      })
-                ],
-              );
-            });
+              ),
+              actions: [
+                ElevatedButton(
+                    child: Text("Add Task"),
+                    onPressed: () {
+                      // your code
+                    })
+              ],
+            );
+          },
+        );
       },
     );
   }
